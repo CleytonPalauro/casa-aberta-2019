60f773b132cce23819c622f2a07d379183a44ccf
Cleyton Palauro <cleytonresc@gmail.com>
2019-09-03 14:45:58 -0300

    Add files via upload

---
+++ Scripts/FinalGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinalGame : MonoBehaviour
+{
+    public GameObject myPlayer;
+
+    // Start is called before the first frame update
+    void Start() { }
+
+    private void OnTriggerEnter(Collider myCollider)
+    {
+        if (myCollider.gameObject == myPlayer)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
---
+++ Scripts/PointsManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PointsManager : MonoBehaviour
+{
+    public static int points;
+
+    public static int maxPoints;
+
+    public int totalPoints = 0;
+
+    public Text canvasText;
+
+    private string tempText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        points = 0;
+        maxPoints = totalPoints;
+        tempText = canvasText.text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        canvasText.text = tempText + points.ToString();
+    }
+
+    public static void UpdatePoints(int somaPonto)
+    {
+        points += somaPonto;
+    }
+}
